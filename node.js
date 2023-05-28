a05a0a5c733d312ec244640a381f35e9f44994fa
sagar9322 <sagarkasundra223@gmail.com>
2023-05-28 17:55:27 +0530

    third-commit

--- node.js
+++ node.js
@@ -1,15 +1,24 @@
 // console.log("Hello World");
 
-const multiply = (a,b) => {
-    return a*b;
-}
-console.log(multiply(2,3));
+// const multiply = (a,b) => {
+//     return a*b;
+// }
+// console.log(multiply(2,3));
 
-const student = {
-    name: "sagar",
-    age: 30,
-    greet: function() {
-        console.log(`Hi, I am ${this.name}`);
+// const student = {
+//     name: "sagar",
+//     age: 30,
+//     greet: function() {
+//         console.log(`Hi, I am ${this.name}`);
+//     }
+// }
+// console.log(student.greet());
+
+const array = ['apple', 'oranges', '', 'mango', '', 'lemon'];
+let newArr = array.map((item) => {
+    if(item == ''){
+        return item = "empty string";
     }
-}
-console.log(student.greet());
+    else return item;
+})
+console.log(newArr);
