658ce6a172b9aa0bc989754ae900b6d00a09a6a6
sagar9322 <sagarkasundra223@gmail.com>
2023-05-28 12:03:08 +0530

    second-commit

--- node.js
+++ node.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// console.log("Hello World");
+
+const multiply = (a,b) => {
+    return a*b;
+}
+console.log(multiply(2,3));
+
+const student = {
+    name: "sagar",
+    age: 30,
+    greet: function() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+}
+console.log(student.greet());
