f2ff8c9283950ad9a5b06f729f6e06c72f1a27f4
sagar9322 <sagarkasundra223@gmail.com>
2023-05-29 12:44:19 +0530

    request-response

--- app.js
+++ app.js
@@ -2,11 +2,39 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log('Hi Sagar');
+  const url = req.url;
+  const method = req.method;
+  if(url === '/home'){
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Enter Massage</title><head>');
+  res.write('<body><form action="/massage" method="POST"><input type="text" name="massage"><button type="submit">send</button></form></body>');
+  res.write('</html>');
+  return res.end("Welcome Home");
+  }
+  if(url === '/about'){
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Enter Massage</title><head>');
+  res.write('<body><form action="/massage" method="POST"><input type="text" name="massage"><button type="submit">send</button></form></body>');
+  res.write('</html>');
+  return res.end("Welcome to about us page");
+  }
+  if(url === '/node'){
+    res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Enter Massage</title><head>');
+  res.write('<body><form action="/massage" method="POST"><input type="text" name="massage"><button type="submit">send</button></form></body>');
+  res.write('</html>');
+  return res.end("Welcome to my Node Js project");
+  }
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Sagar');
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Node Page</title><head>');
+  res.write('<body><h1>Hello Sagar</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(4000);
