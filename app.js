73cfede57ad25da56f34562b6d0e89a11013bead
sagar9322 <sagarkasundra223@gmail.com>
2023-05-31 09:38:36 +0530

    express-middleware

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    console.log("in the middleware!");
+    next();
+});
 
-const http = require('http');
+app.use((req, res, next) => {
+    console.log("in another middleware!");
+    res.send('<h1>Hello Fron Express!</h1>');
+});
 
-const routes = require('./routes');
-const server = http.createServer(routes);
+app.listen(3000);
 
-server.listen(3000);
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+
+// const routes = require('./routes');
+// const server = http.createServer(routes);
+
+// server.listen(3000);
+// const http = require("http");
+// const fs = require("fs");
+// const server = http.createServer((req, res) => {
+//     const url = req.url;
+//     if(url === "/sagar"){
+//         res.write(`<html><body><div><h1>hi Sagar</h1></div></body></html>`);
+//     }
+//     console.log(url);
+// })
+// const message = fs.readFileSync("message.txt");
+// console.log(message);
+// server.listen(3000);
